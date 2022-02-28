532f019bc789bc9a3e2bc01844a9857a362f4041
SylvanSign <jake.d.bray@gmail.com>
2022-02-27 20:07:39 -0500

    working without git on GNU+Linux

--- scenes/PckDownloader.gd
+++ scenes/PckDownloader.gd
@@ -11,65 +11,12 @@ const CARDS_RES_LOCATION := 'res://cards'
 const BUNDLED_DECKS := 'res://decks/'
 const CHALLENGE_DECKS := 'user://decks/challenge'
 const GIT_ZIPPED := 'user://cards.zip'
-const GIT_UNZIPPED := 'user://MeCCG-Windows-EN-master'
-const GIT_ASSET_DIR_PATH := 'user://MeCCG-Windows-EN-master/cards/metw/graphics/Metw'
+const GIT_UNZIPPED := 'user://cards'
+const GIT_ASSET_DIR_PATH := 'user://cards/MeCCG-Windows-EN-master/metw/graphics/Metw'
 
 func _ready() -> void:
-	check_for_cards()
 	setup_decks_dir()
-
-
-func check_for_cards() -> void:
-	if ProjectSettings.load_resource_pack(CARDS_PCK):
-		get_tree().change_scene("res://scenes/DeckBuilder/DeckBuilder.tscn")
-	else:
-		label.text = 'Missing card assets. Would you like to download them?'
-		message.visible = true
-		download_button.visible = true
-
-
-func _on_DownloadButton_pressed() -> void:
-	download_button.visible = false
-	if Directory.new().file_exists('user://cards.zip'):
-		print('already downloaded cards.zip, so need to unzip...')
-		label.text = 'cards.zip found! Will try to unzip...'
-		_on_HTTPRequest_request_completed(0, 200, [], []) # TODO this is a bit ugly...
-	else:
-		label.text = 'Downloading cards. This may take a while...'
-		http_request.download_file = 'user://cards.zip'
-		http_request.request('https://github.com/semaex/MeCCG-Windows-EN/archive/refs/heads/master.zip')
-
-
-func _on_HTTPRequest_request_completed(_result: int, response_code: int, _headers: PoolStringArray, _body: PoolByteArray) -> void:
-	if response_code != 200:
-		var err_msg := 'Got HTTP response ' + str(response_code)
-		printerr(err_msg)
-		OS.alert(err_msg, 'Bad HTTP response')
-		get_tree().quit(1)
-	else:
-		label.text = 'Unzipping files...'
-		yield(get_tree(), "idle_frame")
-
-		match OS.get_name():
-			'Windows':
-				pass
-			_:
-				unix_like_unzip()
-
-		label.text = 'Building pck file from cards...'
-		yield(get_tree(), "idle_frame")
-		pack()
-
-
-func unix_like_unzip() -> void:
-	var output = []
-	var exit_code := OS.execute('unzip', [fs_path('user://cards.zip'), '-d', fs_path('user://')], true, output, true)
-	if exit_code != OK:
-		printerr('unzip failed with code ' + str(exit_code))
-		printerr(output)
-		OS.alert("Please install 'unzip' or manually unzip 'cards.zip' and run again!", 'Failed to unzip!')
-		OS.shell_open(OS.get_user_data_dir())
-		get_tree().quit(1)
+	check_for_cards()
 
 
 func setup_decks_dir() -> void:
@@ -85,10 +32,56 @@ func setup_decks_dir() -> void:
 	dir.list_dir_begin(true, true)
 	var deck = dir.get_next()
 	while deck != "":
-		print('trying to copy ', deck)
 		if OK != dir.copy(BUNDLED_DECKS.plus_file(deck), CHALLENGE_DECKS.plus_file(deck)):
 			printerr('Error copying ', deck)
 		deck = dir.get_next()
+
+
+func check_for_cards() -> void:
+	if ProjectSettings.load_resource_pack(CARDS_PCK):
+		get_tree().change_scene("res://scenes/DeckBuilder/DeckBuilder.tscn")
+	else:
+		label.text = 'Missing card assets. Would you like to download them?'
+		message.visible = true
+		download_button.visible = true
+
+
+func _on_DownloadButton_pressed() -> void:
+	download_button.visible = false
+	if Directory.new().file_exists('user://cards.zip'):
+		print('already downloaded cards.zip, so need to unzip...')
+		_on_HTTPRequest_request_completed(0, 200, [], []) # TODO this is a bit ugly...
+	else:
+		label.text = 'Downloading cards. This may take a while...'
+		http_request.request('https://github.com/semaex/MeCCG-Windows-EN/archive/refs/heads/master.zip')
+
+
+func _on_HTTPRequest_request_completed(_result: int, response_code: int, _headers: PoolStringArray, _body: PoolByteArray) -> void:
+	if response_code != 200:
+		var err_msg := 'Got HTTP response ' + str(response_code)
+		printerr(err_msg)
+		OS.alert(err_msg, 'Bad HTTP response')
+		get_tree().quit(1)
+	else:
+		if Directory.new().file_exists('user://cards'):
+			print('files already unzipped')
+		else:
+			label.text = 'Unzipping files...'
+			yield(get_tree().create_timer(0.3), "timeout")
+
+			match OS.get_name():
+				'Windows':
+					pass
+				_:
+					if !run('unzips '+fs_path('user://cards.zip')+' -d '+fs_path('user://cards')):
+						OS.alert("Please install 'unzip' or manually unzip 'cards.zip' and run again!", 'Failed to unzip!')
+						OS.shell_open(OS.get_user_data_dir())
+						get_tree().quit(1)
+
+			label.text = 'Building pck file from cards...'
+			yield(get_tree().create_timer(0.3), "timeout")
+		pack()
+
 
 
 func pack() -> void:
@@ -96,6 +89,7 @@ func pack() -> void:
 	assert(OK == packer.pck_start(CARDS_PCK))
 	pack_helper(packer, GIT_ASSET_DIR_PATH, CARDS_RES_LOCATION)
 	assert(OK == packer.flush(true))
+	rm_rf(fs_path(GIT_ZIPPED))
 	rm_rf(fs_path(GIT_UNZIPPED))
 	check_for_cards()
 
@@ -115,7 +109,6 @@ func pack_helper(packer: PCKPacker, fs_path: String, pack_path: String) -> void:
 		file_name = dir.get_next()
 
 
-
 func rm_rf(path: String) -> void:
 	match OS.get_name():
 		'Windows':
@@ -133,7 +126,7 @@ func fs_path(path: String) -> String:
 			return globalized_path
 
 
-func run(command: String) -> void:
+func run(command: String) -> bool:
 	var args := Array(command.split(' ', false))
 	var path = args.pop_front()
 
@@ -142,6 +135,9 @@ func run(command: String) -> void:
 	if exit_code != OK:
 		printerr('Command '+command+' failed with code '+str(exit_code))
 		printerr(output)
+		return false
+	else:
+		return true
 
 
 func _on_GitMessage_meta_clicked(meta: String) -> void:
