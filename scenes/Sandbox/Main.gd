e4bed93ee2fe38e1bbbdd90551b227713d990cff
SylvanSign <jake.d.bray@gmail.com>
2022-04-09 11:25:37 -0400

    add mouse panning

---
+++ scenes/Sandbox/Main.gd
@@ -0,0 +1,4 @@
+extends TextureRect
+
+func _ready() -> void:
+	texture = Global.get_texture_from_cards_pck('map1.png')
--- scenes/Sandbox/Player.gd
+++ scenes/Sandbox/Player.gd
@@ -3,6 +3,8 @@ extends Camera2D
 export(int) var speed := 1000
 export(float) var zoom_factor := 1.5
 
+var panning := false
+
 func _process(delta: float) -> void:
 	var input := Input.get_vector('left', 'right', 'up', 'down')
 	position += input * speed * zoom * delta
@@ -14,3 +16,9 @@ func _input(event: InputEvent) -> void:
 	# zoom out
 	elif event.is_action_pressed('scroll_down'):
 		zoom *= zoom_factor
+	elif event.is_action_pressed('middle_click'):
+		panning = true
+	elif event.is_action_released('middle_click'):
+		panning = false
+	elif panning and event is InputEventMouseMotion:
+		position += event.relative * speed * zoom * get_process_delta_time()
